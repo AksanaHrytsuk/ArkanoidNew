6bbbb0870271eca28059343fb2c2dcaab433fe65
AksanaHrytsuk <30596314+AksanaHrytsuk@users.noreply.github.com>
2020-03-05 21:18:57 +0300

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public int speed;
+    bool started; // false
+    Platform platform;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        started = false;
+        platform = FindObjectOfType<Platform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (started)
+        {
+            // ничего неделает
+        }
+        else
+        {
+            LockBallTOPlatform();
+        }
+    }
+        private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("CollisionEnter");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("CollisionExit");
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("CollisionStay");
+    }
+
+
+    private void LockBallTOPlatform()
+    {
+        // ldbufnmcz lfkmit
+        transform.position = new Vector3(platform.transform.position.x, transform.position.y, 0);
+        if (Input.GetMouseButtonDown(0))
+        {
+            started = true;
+            LaunchBall();
+        }
+    }
+    private void LaunchBall()
+    {
+        Vector2 force = new Vector2(speed, speed);
+        rb.AddForce(force);
+    }
+}
+
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float minX = -7f;
+    public float maxX = 7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        
+        Camera.main.ScreenToWorldPoint(mousePos);
+        float mouseX = mouseWorldPos.x;
+        // Debug.Log("mousePos: " + mousePos + "mousWorldPos" + mousWorldPos);
+        float clampedMouseX = Mathf.Clamp(mouseX, minX, maxX);
+        float platformY = transform.position.y;
+        transform.position = new Vector3(clampedMouseX, platformY, 0);
+
+    }
+}
